dc6601535316431f332143c361551b8e5e1672a6
izaiasn <61168322+izaiasn@users.noreply.github.com>
2023-10-17 11:20:54 -0300

    Add files via upload

--- src/main/java/call/care/api/controller/EquipamentoController.java
+++ src/main/java/call/care/api/controller/EquipamentoController.java
@@ -2,11 +2,14 @@ package call.care.api.controller;
 import call.care.api.equipamento.*;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import org.apache.catalina.LifecycleState;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("equipamento")
@@ -20,5 +23,12 @@ public class EquipamentoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastrarEquipamento dados) {
         repository.save(new Equipamento(dados));
+    }
+// Por ser um metodo de listar não utiliza o post mas List
+    @GetMapping
+    public List<DadosListaEquipamento> listar(){
+        return repository.findAll().stream().map(DadosListaEquipamento::new ).toList();
+
+
     }
 }
