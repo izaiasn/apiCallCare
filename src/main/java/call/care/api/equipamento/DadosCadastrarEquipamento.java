d3f1581d13826508b9e496e6002a620ef32a4d4e
izaiasn <61168322+izaiasn@users.noreply.github.com>
2023-10-17 11:20:05 -0300

    Add files via upload

--- src/main/java/call/care/api/equipamento/DadosCadastrarEquipamento.java
+++ src/main/java/call/care/api/equipamento/DadosCadastrarEquipamento.java
@@ -1,24 +1,27 @@
 package call.care.api.equipamento;
 
+import jakarta.validation.constraints.NotBlank;
+import org.antlr.v4.runtime.misc.NotNull;
+
 public record DadosCadastrarEquipamento(
 
 
          Long id,
-
+         @NotBlank
          String modelo,
-
+         @NotBlank
          String tipoequipamento,
-
+         @NotBlank
          String fabricante,
-
+         @NotBlank
          String modalidade,
-
+         @NotBlank
          String numeroserie,
-
+         @NotBlank
          String codigoequipamento,
-
+         @NotBlank
          String datainstalacao,
-
+         @NotBlank
          String hora,
 
          Boolean ativo) {
